14f83858665c6cd00983d6e345e1d7516d053106
anticlimactic <anticlimactic@users.noreply.github.com>
2022-03-23 14:55:35 +1100

    test: add migration test

--- src/test/StrategyMigration.t.sol
+++ src/test/StrategyMigration.t.sol
@@ -15,21 +15,53 @@ contract StrategyMigrationTest is StrategyFixture {
     // Use another copy of the strategy to simmulate the migration
     // Show that nothing is lost.
     function testMigration(uint256 _amount) public {
-        vm_std_cheats.assume(_amount > 0.1 ether && _amount < 10e18);
+        vm_std_cheats.assume(
+            _amount > 0.1 ether && _amount < 100_000_000 ether
+        );
+        tip(address(want), user, _amount);
 
         // Deposit to the vault and harvest
-        vm_std_cheats.prank(user);
-        want.approve(address(vault), _amount);
-        vm_std_cheats.prank(user);
-        vault.deposit(_amount);
+        actions.userDeposit(user, vault, want, _amount);
         skip(1);
+        vm_std_cheats.prank(gov);
         strategy.harvest();
-        assertEq(strategy.estimatedTotalAssets(), _amount);
+        assertRelApproxEq(strategy.estimatedTotalAssets(), _amount, DELTA);
 
-        // Migrate to a new strategy
+        uint256 preWantBalance = want.balanceOf(address(strategy));
+
         vm_std_cheats.prank(strategist);
-        address newStrategyAddr = deployStrategy(address(vault));
-        vault.migrateStrategy(address(strategy), newStrategyAddr);
-        assertEq(Strategy(newStrategyAddr).estimatedTotalAssets(), _amount);
+        Strategy newStrategy = Strategy(deployStrategy(address(vault)));
+        tip(address(weth), whale, 1e6);
+        vm_std_cheats.prank(whale);
+        weth.transfer(address(newStrategy), 1e6);
+
+        // migration with more than dust reverts, there is no way to transfer the debt position
+        vm_std_cheats.prank(gov);
+        vm_std_cheats.expectRevert(bytes(""));
+        vault.migrateStrategy(address(strategy), address(newStrategy));
+
+        vm_std_cheats.prank(gov);
+        vault.revokeStrategy(address(strategy));
+        skip(1);
+        vm_std_cheats.prank(gov);
+        strategy.harvest();
+
+        vm_std_cheats.prank(gov);
+        vault.migrateStrategy(address(strategy), address(newStrategy));
+        vm_std_cheats.prank(gov);
+        vault.updateStrategyDebtRatio(address(newStrategy), 10_000);
+        skip(1);
+        vm_std_cheats.prank(gov);
+        newStrategy.harvest();
+
+        assertRelApproxEq(newStrategy.estimatedTotalAssets(), _amount, DELTA);
+        assertRelApproxEq(
+            preWantBalance,
+            want.balanceOf(address(newStrategy)),
+            DELTA
+        );
+
+        vm_std_cheats.prank(gov);
+        newStrategy.harvest();
     }
 }
